59c4ec8440ac077912bb99e11372a67eb10d1c34
Ayannuga Gbenga <65336545+gbbest15@users.noreply.github.com>
2023-10-12 19:54:20 +0100

    wash review

--- lib/features/customer/views/home/watch/presentation/review.dart
+++ lib/features/customer/views/home/watch/presentation/review.dart
@@ -42,57 +42,55 @@ class WashReview extends StatelessWidget {
                         fontWeight: FontWeight.bold, fontSize: 23),
                   ),
                   Gap(20.h),
+                ],
+              ),
+              Gap(25.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Row(
-                        children: [
-                          Text(
-                            "Ace Wash",
-                            style: Theme.of(context)
-                                .primaryTextTheme
-                                .headlineMedium!
-                                .copyWith(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 14.sp),
-                          ),
-                          const Gap(15),
-                          Text(
-                            "4.5",
-                            style: Theme.of(context)
-                                .primaryTextTheme
-                                .headlineMedium!
-                                .copyWith(
-                                    color: const Color(0xff525355),
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 14.sp),
-                          ),
-                          const Gap(5),
-                          const Icon(
-                            Icons.star,
-                            color: Colors.red,
-                            size: 20,
-                          )
-                        ],
-                      ),
                       Text(
-                        "300 ratings",
-                        style:
-                            Theme.of(context).primaryTextTheme.headlineMedium!,
+                        "Ace Wash",
+                        style: Theme.of(context)
+                            .primaryTextTheme
+                            .headlineMedium!
+                            .copyWith(
+                                fontWeight: FontWeight.bold, fontSize: 14.sp),
+                      ),
+                      const Gap(15),
+                      Text(
+                        "4.5",
+                        style: Theme.of(context)
+                            .primaryTextTheme
+                            .headlineMedium!
+                            .copyWith(
+                                color: const Color(0xff525355),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14.sp),
+                      ),
+                      const Gap(5),
+                      const Icon(
+                        Icons.star,
+                        color: Colors.red,
+                        size: 20,
                       )
                     ],
                   ),
-                  Gap(20.h),
-                  ListView.separated(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemBuilder: (context, index) => const ReviewWidget(),
-                    separatorBuilder: (context, index) => const Gap(20),
-                    itemCount: 3,
+                  Text(
+                    "300 ratings",
+                    style: Theme.of(context).primaryTextTheme.headlineMedium!,
                   )
                 ],
               ),
               Gap(25.h),
+              ListView.separated(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) => const ReviewWidget(),
+                separatorBuilder: (context, index) => const Gap(20),
+                itemCount: 3,
+              )
             ],
           )),
     );
@@ -105,44 +103,50 @@ class ReviewWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: 150,
       decoration: BoxDecoration(
-        color: const Color(0xff525355).withAlpha(60),
+        color: const Color(0xff525355).withAlpha(40),
         borderRadius: BorderRadius.circular(7),
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Amanda",
-                style: Theme.of(context).primaryTextTheme.headlineMedium!,
-              ),
-              RatingBar.builder(
-                initialRating: 3,
-                minRating: 1,
-                direction: Axis.horizontal,
-                allowHalfRating: true,
-                itemCount: 5,
-                itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
-                itemBuilder: (context, _) => const Icon(
-                  Icons.star,
-                  color: Colors.red,
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Amanda",
+                  style: Theme.of(context).primaryTextTheme.headlineMedium!,
                 ),
-                onRatingUpdate: (rating) {},
-              ),
-            ],
-          ),
-          Text(
-            "This was my best laundry experience, thank you Ace wash!",
-            style: Theme.of(context).primaryTextTheme.headlineMedium!,
-          ),
-          Text(
-            "12/06/23",
-            style: Theme.of(context).primaryTextTheme.headlineMedium!,
-          )
-        ],
+                RatingBar.builder(
+                  itemSize: 20,
+                  initialRating: 3,
+                  minRating: 1,
+                  direction: Axis.horizontal,
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  itemPadding: EdgeInsets.zero,
+                  itemBuilder: (context, _) => const Icon(
+                    Icons.star,
+                    color: Colors.red,
+                  ),
+                  onRatingUpdate: (rating) {},
+                ),
+              ],
+            ),
+            Text(
+              "This was my best laundry experience, thank you Ace wash!",
+              style: Theme.of(context).primaryTextTheme.headlineMedium!,
+            ),
+            Text(
+              "12/06/23",
+              style: Theme.of(context).primaryTextTheme.headlineMedium!,
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/features/customer/views/home/watch/presentation/widget/selected_dryclean.dart
+++ lib/features/customer/views/home/watch/presentation/widget/selected_dryclean.dart
@@ -14,120 +14,117 @@ class SelectedDryClean extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          ListView.separated(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemBuilder: (context, index) => Container(
-                    decoration: BoxDecoration(
-                      color: AppColors.gray.withOpacity(0.3),
-                      borderRadius: BorderRadius.circular(15.r),
-                    ),
-                    child: Theme(
-                      data: Theme.of(context)
-                          .copyWith(dividerColor: Colors.transparent),
-                      child: ExpansionTile(
-                        key: Key(index.toString()), //attention
-                        initiallyExpanded: index == index,
-                        leading: const SvgImage(
-                          asset: MyStrings.washApealIcons,
-                          color: Colors.black,
-                          height: 20,
-                          width: 20,
-                        ),
-                        title: Text(
-                          MyStrings.washApeal,
-                          style: Theme.of(context)
-                              .primaryTextTheme
-                              .headlineMedium!,
-                        ),
-                        childrenPadding:
-                            const EdgeInsets.symmetric(horizontal: 10),
-                        children: List.generate(
-                          index == 0 ? 3 : 1,
-                          (index) => const Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: CartWidget(),
-                          ),
-                        ).toList(),
+    return Column(
+      children: [
+        ListView.separated(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemBuilder: (context, index) => Container(
+                  decoration: BoxDecoration(
+                    color: AppColors.gray.withOpacity(0.3),
+                    borderRadius: BorderRadius.circular(15.r),
+                  ),
+                  child: Theme(
+                    data: Theme.of(context)
+                        .copyWith(dividerColor: Colors.transparent),
+                    child: ExpansionTile(
+                      key: Key(index.toString()), //attention
+                      initiallyExpanded: index == index,
+                      leading: const SvgImage(
+                        asset: MyStrings.washApealIcons,
+                        color: Colors.black,
+                        height: 20,
+                        width: 20,
                       ),
+                      title: Text(
+                        MyStrings.washApeal,
+                        style:
+                            Theme.of(context).primaryTextTheme.headlineMedium!,
+                      ),
+                      childrenPadding:
+                          const EdgeInsets.symmetric(horizontal: 10),
+                      children: List.generate(
+                        index == 0 ? 3 : 1,
+                        (index) => const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: CartWidget(),
+                        ),
+                      ).toList(),
                     ),
                   ),
-              separatorBuilder: (context, index) => const Gap(20),
-              itemCount: 2),
-          const Gap(10),
-          GestureDetector(
-            onTap: () => getx.Get.to(() => const AddItemWash()),
-            child: Row(
-              children: [
-                const Icon(
-                  Icons.add,
-                  color: Color(0xff008080),
-                  size: 30,
                 ),
-                const Gap(10),
-                Text(
-                  "Add item",
-                  style: Theme.of(context)
-                      .primaryTextTheme
-                      .headlineMedium!
-                      .copyWith(color: const Color(0xff008080), fontSize: 18),
-                )
-              ],
+            separatorBuilder: (context, index) => const Gap(20),
+            itemCount: 2),
+        const Gap(10),
+        GestureDetector(
+          onTap: () => getx.Get.to(() => const AddItemWash()),
+          child: Row(
+            children: [
+              const Icon(
+                Icons.add,
+                color: Color(0xff008080),
+                size: 30,
+              ),
+              const Gap(10),
+              Text(
+                "Add item",
+                style: Theme.of(context)
+                    .primaryTextTheme
+                    .headlineMedium!
+                    .copyWith(color: const Color(0xff008080), fontSize: 18),
+              )
+            ],
+          ),
+        ),
+        const Gap(15),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Total (4)",
+              style: Theme.of(context)
+                  .primaryTextTheme
+                  .headlineMedium!
+                  .copyWith(fontSize: 18, fontWeight: FontWeight.w700),
             ),
-          ),
-          const Gap(15),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Total (4)",
-                style: Theme.of(context)
-                    .primaryTextTheme
-                    .headlineMedium!
-                    .copyWith(fontSize: 18, fontWeight: FontWeight.w700),
-              ),
-              Text(
-                "N3,000",
-                style: Theme.of(context)
-                    .primaryTextTheme
-                    .headlineMedium!
-                    .copyWith(fontSize: 18, fontWeight: FontWeight.w700),
-              )
-            ],
-          ),
-          const Gap(15),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Estimated time",
-                style: Theme.of(context)
-                    .primaryTextTheme
-                    .headlineMedium!
-                    .copyWith(fontSize: 18, fontWeight: FontWeight.w700),
-              ),
-              Text(
-                "3 days",
-                style: Theme.of(context)
-                    .primaryTextTheme
-                    .headlineMedium!
-                    .copyWith(fontSize: 18, fontWeight: FontWeight.w700),
-              )
-            ],
-          ),
-          const Gap(15),
-          ActionCustomButton(
-              title: "Schedule",
-              isLoading: false,
-              onclick: () {
-                FocusScope.of(context).unfocus();
-                getx.Get.to(() => const AddItemWash());
-              }),
-        ],
-      ),
+            Text(
+              "N3,000",
+              style: Theme.of(context)
+                  .primaryTextTheme
+                  .headlineMedium!
+                  .copyWith(fontSize: 18, fontWeight: FontWeight.w700),
+            )
+          ],
+        ),
+        const Gap(15),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Estimated time",
+              style: Theme.of(context)
+                  .primaryTextTheme
+                  .headlineMedium!
+                  .copyWith(fontSize: 18, fontWeight: FontWeight.w700),
+            ),
+            Text(
+              "3 days",
+              style: Theme.of(context)
+                  .primaryTextTheme
+                  .headlineMedium!
+                  .copyWith(fontSize: 18, fontWeight: FontWeight.w700),
+            )
+          ],
+        ),
+        const Gap(15),
+        ActionCustomButton(
+            title: "Schedule",
+            isLoading: false,
+            onclick: () {
+              FocusScope.of(context).unfocus();
+              getx.Get.to(() => const AddItemWash());
+            }),
+      ],
     );
   }
 }
