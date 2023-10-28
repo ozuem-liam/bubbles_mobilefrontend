3a902476dc4a6869bb4e95cab3adbe7153e78b67
Ayannuga Gbenga <65336545+gbbest15@users.noreply.github.com>
2023-10-28 13:38:04 +0100

    update

---
+++ lib/features/customer/views/home/order_Navgivation/cashout/selected_oreder_details.dart
@@ -0,0 +1,18 @@
+import 'package:bubbles/utils/constvalues.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SelectedOrdersDetails extends StatelessWidget {
+  const SelectedOrdersDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          minimum: EdgeInsets.symmetric(horizontal: generalHorizontalPadding.w),
+          child: Column(
+            children: [],
+          )),
+    );
+  }
+}
---
+++ lib/features/customer/views/home/order_Navgivation/widgets/orders_selected.dart
@@ -0,0 +1,124 @@
+import 'package:bubbles/style/appColors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gap/gap.dart';
+
+class OrdersSelected extends StatelessWidget {
+  const OrdersSelected({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final styleTheme = Theme.of(context).primaryTextTheme.headlineMedium!;
+    return Column(
+      children: [
+        Container(
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: const Color(0xff525355).withAlpha(60),
+            ),
+            borderRadius: BorderRadius.circular(15.r),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Shirt (2), Trouser (1), Socks (1)",
+                      style: styleTheme.copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    Container(
+                      height: 30,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        color: AppColors.gray.withOpacity(0.3),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Processing",
+                          style: styleTheme,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Delivery",
+                      style: styleTheme.copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "N1,000",
+                      style: styleTheme.copyWith(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        const Gap(20),
+        Container(
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: const Color(0xff525355).withAlpha(60),
+            ),
+            borderRadius: BorderRadius.circular(15.r),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Shirt (2), Trouser (1), Socks (1)",
+                      style: styleTheme.copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    Container(
+                      height: 30,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        color: AppColors.gray.withOpacity(0.3),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Processing",
+                          style: styleTheme,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Delivery",
+                      style: styleTheme.copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "N1,000",
+                      style: styleTheme.copyWith(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
